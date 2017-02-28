84174aac5a60652c8a5c177313ce605651ddb392
Kumar <shivam.b.kumar@capgemini.com>
2017-02-28 13:35:27 +0530

    hello file added

---
+++ hello.java
@@ -0,0 +1,19 @@
+class hello
+{
+	int id;
+	String name;
+
+	public void detaiil(){
+
+		name="shivam";
+	id="75557";
+	System.out.println(name+ " " +id);
+	}
+
+	public static void main(String args[])
+	{
+		hello h =new hello();
+		h.detail();
+	}
+}
+
